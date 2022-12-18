f7fb29d00b35ff95186d35630d1ddd60b79dafa4
alperenkaptan <alperen9792@gmail.com>
2022-12-18 19:08:24 +0300

    deunde server implementation identity auth

---
+++ Restaurant.Identity/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Identity;
+using Restaurant.Identity.DbContexts;
+using Restaurant.Identity.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+var buildery = builder.Services.AddIdentityServer(opt =>
+{
+    opt.Events.RaiseErrorEvents = true;
+    opt.Events.RaiseInformationEvents = true;
+    opt.EmitStaticAudienceClaim = true;
+    opt.Events.RaiseFailureEvents = true;
+    opt.Events.RaiseSuccessEvents = true;
+}).AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+buildery.AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Restaurant.Identity/SD.cs
@@ -0,0 +1,58 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Identity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope> {
+                new ApiScope("Restaurant", "Restaurant Server"),
+                new ApiScope(name: "read",   displayName: "Read your data."),
+                new ApiScope(name: "write",  displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data.")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+                    ClientSecrets= { new Secret("secrety".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes={ "read", "write","profile"}
+                },
+                new Client
+                {
+                    ClientId="Restaurant",
+                    ClientSecrets= { new Secret("secrety".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris={ "http://localhost:38655/signin-oidc" },
+                    PostLogoutRedirectUris={"http://localhost:38655/signout-callback-oidc" },
+                    AllowedScopes=new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "Restaurant"
+                    }
+                },
+            };
+    }
+}
